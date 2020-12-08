47de4735d3868c693bb9772483cb7ba191b719f5
myxomopx <myxomopx@gmail.com>
2020-12-08 19:43:06 +0500

    Removed router

--- src/ui/components/app/App.tsx
+++ src/ui/components/app/App.tsx
@@ -1,9 +1,5 @@
 import * as React from "react";
 import {FC} from "react";
-import { UIRouter, UIView, pushStateLocationPlugin } from "@uirouter/react";
-// import {RP5NavBar} from "./navigation/RP5NavBar";
-import {configRouter} from "../../router/configRouter";
-import {routerStates} from "../../router/states";
 
 import "./app.scss"
 
--- src/ui/router/states.ts
+++
@@ -1,9 +0,0 @@
-import {MainMapPage} from "../components/page/main-map/MainMapPage";
-
-export const routerStates = [
-    {
-        name: "main",
-        url: "/",
-        component: MainMapPage
-    }
-]
